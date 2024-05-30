82658dfaf7b2cff3320f6d67711c8160b695cc46
Nsamba Taufeeq <nsambataufeeq@gmail.com>
2024-05-30 12:01:32 +0300

    added publication store

--- app/Http/Controllers/PublicationsController.php
+++ app/Http/Controllers/PublicationsController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Publication;
 use App\PublicationBuyer;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Artisan;
+use App\FileType;
 
 class PublicationsController extends Controller
 {
@@ -44,6 +47,7 @@ class PublicationsController extends Controller
     public function create()
     {
         //
+        return view('publications.create');
     }
 
     /**
@@ -52,6 +56,49 @@ class PublicationsController extends Controller
     public function store(Request $request)
     {
         //
+        $this->authorize('create egazette');
+        $user = $request->user();
+        $publication = Publication::create([
+            "pub_title"=>$request->input("pub_title"),
+            "pub_fees"=>$request->input("pub_fees"),
+            "pub_author"=>$request->input("pub_author"),
+            "created_by"=>$user->id,
+            "name"=>"Uganda Gazette issued ".$request->input("gaz_issue_no"),
+            "status"=>config('constants.GAZETTE_STATUSES.PUBLISHED'),
+            "category"=>config('constants.DOC_TYPES.PUBLICATION')
+        ]);
+        $publication->newActivity('Publication uploaded by: '.$user->name);
+        
+        $password = Str::random(30);
+        $publication->pub_key = $password; 
+        $publication->save();
+        // save file.
+        $uploaded_file = $request->file('file_scan');
+        $stored_path = $uploaded_file->store('files/original');
+
+        // Get the full path
+        $full_path = storage_path('app/' . $stored_path);
+
+        
+        $new_name = Str::random(50).".pdf";
+        $result = str_replace($uploaded_file->hashName(),$new_name,$full_path);
+        Artisan::call('app:encrypt-pdf',[
+            'inputFile' => $full_path,
+            'outputFile' => $result,
+            'userPassword' => $password
+        ]);
+
+        $fileData['name'] =  $uploaded_file->getClientOriginalName();
+        $fileData['created_by'] = $user->id;
+        $fileData['file'] = $new_name;
+        $fileData['custom_fields'] = json_encode([]);
+        $fileData['created_at'] = now();
+        $fileData['updated_at'] = now();
+        $file_type = FileType::where('name', "Gazette")->first();
+        $fileData['file_type_id'] = $file_type->id;
+        $fileData['document_id'] = $publication->id;
+        $publication->files()->insert([$fileData]);
+        return redirect()->route('publications.index');
     }
 
     /**
---
+++ resources/views/publications/create.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+@section('title', 'Publications Upload')
+@section('content')
+<section class="content-header">
+    <h1>
+        Add a Publication
+    </h1>
+</section>
+<div class="content">
+    <div class="box box-primary" style="width: 440px; margin-left:auto; margin-right:auto;">
+        <div class="box-body">
+            <h3>Upload Publication</h3>
+            <div class="row">
+                {!! Form::open(['route' => 'publications.store','files'=>true]) !!}
+                <div class="col-md-12">
+                    <div class="form-group">
+                        {!! Form::label('pub_title', 'Title:') !!}
+                        {!! Form::text('pub_title','', ['class' => 'form-control', 'required' => 'required']) !!}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('pub_fees', 'Fees:') !!}
+                        {!! Form::number('pub_fees',0, ['class' => 'form-control', 'required' => 'required']) !!}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::label('pub_author', 'Author') !!}
+                        {!! Form::date('pub_author','', ['class' => 'form-control', 'required' => 'required']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label for="file" class="control-label">Scanned PDF</label>
+                        {!! Form::file('file_scan', ['class'=>'form-control']) !!} <!-- File upload field -->
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-offset-8 col-md-4">
+                            {!! Form::submit('Submit', ['class' => 'btn btn-primary pull-right']) !!}
+                        </div>
+                    </div>
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
