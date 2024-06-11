183295d15c81b35ef8975a6a2639adb83dc0921b
Nsamba Taufeeq <nsambataufeeq@gmail.com>
2024-06-11 12:07:25 +0300

    grid publications

--- resources/views/publications/index.blade.php
+++ resources/views/publications/index.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 @section('title', 'Publications')
+@section("css")
+<style>
+    :root{
+        --columns: 4;
+    }
+    .grid-container{
+  display: grid;
+  grid-template-columns: repeat(var(--columns), 1fr);
+  gap: 0;
+  list-style-type: none;
+  padding: 0;
+}
+    .thumbnail img {
+    width: 50%;
+    height: auto;
+    border-radius: 8px;
+}
+
+</style>
+@stop
 @section("content")
 <section class="content-header">
     <h1 class="pull-left"></h1>
@@ -32,32 +52,24 @@
                     </div>
                 </div>
                 <div class="box-body">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Price</th>
-                                <th>Author</th>
-                                <th>Created at</th>
-                                <th>Action</th> 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($documents as $document)
-                            <tr>
-                                <td>{{ $document->pub_title }}</td>
-                                <td>{{ $document->pub_fees }}</td>
-                                <td>{{ $document->pub_author }}</td>
-                                <td>{{ $document->created_at }}</td>
-                                <td>
-                                    <a href="{{ route('publications.show', ['publication' => $document->id]) }}" class="btn btn-primary">
-                                        <i class="fa fa-eye"></i> View More
-                                    </a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                    <div class="grid-container">
+                        @foreach($documents as $document)
+                        <div>
+                            <div class="thumbnail">
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/book{{ str_pad(rand(1, 34), 2, '0', STR_PAD_LEFT) }}.jpg" alt="Book" style="margin-top:10px;">
+                                <div class="caption text-center">
+                                    <h5 class="title">{{ $document->pub_title }} by {{$document->pub_author}}</h6>
+                                    <p class="text-danger"><b> UGX. {{ $document->pub_fees }}</b></p>
+                                    <p>
+                                        <a href="{{ route('publications.show', ['publication' => $document->id]) }}" class="btn btn-primary" role="button">
+                                            <i class="fa fa-eye"></i> View More
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
                 </div>
                 <div class="box-footer clearfix">
                     {{ $documents->links() }}
